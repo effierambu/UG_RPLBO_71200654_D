79c240cd373b49fe20cce0f74d48d20cd08fce35
effierambu <78496059+effierambu@users.noreply.github.com>
2022-03-02 17:59:08 +0700

    Add files via upload

---
+++ PRAK RPL-BO/src/main/java/com/ug4/soal1/Dutabus.java
@@ -0,0 +1,9 @@
+package com.ug4.soal1;
+
+public class Dutabus {
+    String driver;
+    double saldo;
+    String getDriver(){return driver;}
+    double getSaldo(){return saldo;}
+
+}
---
+++ PRAK RPL-BO/src/main/java/com/ug4/soal1/Main.java
@@ -0,0 +1,13 @@
+package com.ug4.soal1;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
